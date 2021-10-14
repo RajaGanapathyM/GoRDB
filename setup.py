93c67b8631abdd6f2b2fded4144539983fd88c41
RajaGanapathyM <rganapathy6@gmail.com>
2021-10-14 14:49:45 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -305,3 +305,17 @@ def show_graph(node_dicts):
     nt.from_nx(G)
     nt.set_edge_smooth('dynamic')
     nt.show('nx.html')
+from distutils.core import setup
+
+setup(
+    name = 'GoRDB',
+    packages = ['GoRDB'],
+    version = 'v.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A lightweight python library for implementing GraphQL on Relational DB Tables in few steps using python dicts. The library is built over strawberry-graphql for creating graphQL schema from dataclasses',
+    author = 'RajaGanapathyM',
+    author_email = 'rganapathy6@gmail.com',
+    url = 'https://github.com/RajaGanapathyM/GoRDB',
+    download_url = 'https://github.com/RajaGanapathyM/GoRDB/archive/refs/tags/v.0.1.tar.gz',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
