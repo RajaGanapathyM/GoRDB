1912d8aaf10e9ee8e9bf7e11167118c419349ae2
RajaGanapathyM <rganapathy6@gmail.com>
2021-10-18 14:41:58 +0530

    V6.1
    Bug Fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from distutils.core import setup
+from setuptools  import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'GoRDB',
     packages = ['GoRDB'],
-    version = 'v.0.1',  # Ideally should be same as your GitHub release tag varsion
+    version = 'V006.1',  # Ideally should be same as your GitHub release tag varsion
     description = 'A lightweight python library for implementing GraphQL on Relational DB Tables in few steps using python dicts. The library is built over strawberry-graphql for creating graphQL schema from dataclasses',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author = 'RajaGanapathyM',
     author_email = 'rganapathy6@gmail.com',
     url = 'https://github.com/RajaGanapathyM/GoRDB',
     download_url = 'https://github.com/RajaGanapathyM/GoRDB/archive/refs/tags/v.0.1.tar.gz',
     keywords = ['GraphQL', 'RDB'],
-    classifiers = [],
+    classifiers = ['License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
 )
